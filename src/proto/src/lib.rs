f0650d9fbea148789aeeed46997e2556a8e03ebd
Ed Cragg <drq.11235@gmail.com>
2020-02-10 17:06:35 +0800

    Update primordial protocol

--- src/proto/src/lib.rs
+++ src/proto/src/lib.rs
@@ -1,14 +1,37 @@
-enum CommandType {
-    Identify,
-    Authenticate,
-    Notice,
-    Message,
-    ServerMessage,
+pub enum CommandType {
+    Identify(Identify),
+    Authenticate(Authenticate),
+    Notice(Notice),
+    Message(Message),
+    ServerMessage(ServerMessage),
+    Backscroll(Backscroll),
 }
 
-
 //#[derive(Serialize, Deserialize)]
-struct RofflMessage<'a> {
-    command: CommandType,
-    content: &'a str
+pub struct Identify<'a> {
+    name: &'a str,
+}
+
+pub struct Authenticate<'a> {
+    password: &'a str,
+}
+
+pub struct Notice<'a> {
+    content: &'a str,
+}
+
+pub struct Message<'a> {
+    content: &'a str,
+}
+
+pub struct ServerMessage<'a> {
+    content: &'a str,
+}
+
+pub struct Ping<'a> {
+    content: &'a str,
+}
+
+pub struct Backscroll<'a> {
+    content: &'a str,
 }
