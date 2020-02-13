1eae8e949e537704896da4337a069866dbfb2e0a
Ed Cragg <drq.11235@gmail.com>
2020-02-13 18:15:56 +0800

    Update comments

--- src/roffl/src/crypto.rs
+++ src/roffl/src/crypto.rs
@@ -1,19 +1,29 @@
 /// Derive a key from passphrase using BLAKE3 as a PBKDF.
+///
+/// This is used for example as a PSK for noise protocol communications, the
+/// password used here being the "server password".
 pub fn derive_key(passphrase: &str, length: usize) -> String {
     unimplemented!()
 }
 
 /// Hash user password using Argon2 hash function.
+///
+/// Used for authenticating users.
 pub fn hash_user_password(password: &str) -> String {
     unimplemented!()
 }
 
 /// Verify user password using Argon2 hash.
+///
+/// Used for authenticating users.
 pub fn verify_user_password(hash: &str, password: &str) -> bool {
     unimplemented!()
 }
 
-/// Encrypt a string
-/// Using random key, encrypted with user password as a key
+/// Encrypt a string.
+///
+/// Using random key, encrypted with user password as a key.
 
-/// Encrypt a block of data
+/// Encrypt a block of data.
+
+/// Generate a hopefully cryptographically secure random encryption key.
