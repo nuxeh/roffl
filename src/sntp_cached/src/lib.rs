a8d80e384677332d2f3a628db99b4f63ab16a513
Ed Cragg <drq.11235@gmail.com>
2020-04-12 00:06:54 +0100

    Continue implementation of cached NTP

--- src/sntp_cached/src/lib.rs
+++ src/sntp_cached/src/lib.rs
@@ -1,25 +1,46 @@
+extern crate failure;
 extern crate sntp_request;
 
-use std::io;
+use failure::Error;
 use std::time::Instant;
-use sntp_request::SntpTimestamp;
+use sntp_request::{SntpRequest, SntpTimestamp};
 
+/// Cached NTP struct
 pub struct SntpCached {
     timestamp: Option<SntpTimestamp>,
     last_sync: Option<Instant>,
 }
 
 impl SntpCached {
-    pub fn sync() -> io::Result<()> {
-        Ok(())
+    /// Get a new cached NTP struct
+    pub fn new() -> Self {
+        SntpCached {
+            timestamp: None,
+            last_sync: None,
+        }
     }
 
-    pub fn get_timestamp() -> SntpTimestamp {
+    /// Trigger an NTP sync
+    pub fn sync(&mut self) -> Result<(), Error> {
+        self.sntp_sync()
+    }
+
+    /// Get an NTP timestamp for now
+    pub fn get_timestamp(self) -> SntpTimestamp {
         unimplemented!();
     }
 
-    pub fn get_unix_time() -> i64 {
+    /// Get UNIX time as an i64 (seconds)
+    pub fn get_unix_time(self) -> i64 {
         42
+    }
+
+    /// Sync NTP timestamp
+    fn sntp_sync(&mut self) -> Result<(), Error> {
+        let sntp = SntpRequest::new();
+        self.timestamp = Some(sntp.get_raw_time()?);
+        self.last_sync = Some(Instant::now());
+        Ok(())
     }
 }
 
