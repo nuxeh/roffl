527a3f875f78774d6218908f5ef77f104fed5a48
Ed Cragg <drq.11235@gmail.com>
2020-04-11 23:20:43 +0100

    WIP sntp_cached

--- src/sntp_cached/src/lib.rs
+++ src/sntp_cached/src/lib.rs
@@ -1,3 +1,28 @@
+extern crate sntp_request;
+
+use std::io;
+use std::time::Instant;
+use sntp_request::SntpTimestamp;
+
+pub struct SntpCached {
+    timestamp: Option<SntpTimestamp>,
+    last_sync: Option<Instant>,
+}
+
+impl SntpCached {
+    pub fn sync() -> io::Result<()> {
+        Ok(())
+    }
+
+    pub fn get_timestamp() -> SntpTimestamp {
+        unimplemented!();
+    }
+
+    pub fn get_unix_time() -> i64 {
+        42
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
