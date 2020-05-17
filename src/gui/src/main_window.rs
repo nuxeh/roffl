9e3a4d1da26ee0fecd73bf639c49d9d4c87761c0
Ed Cragg <ed.cragg@eipi.xyz>
2020-05-17 18:58:39 +0100

    Split main window code

--- src/gui/src/main_window.rs
+++ src/gui/src/main_window.rs
@@ -1,12 +1,3 @@
-//! `roffl` main window
-
-mod widgets;
-use widgets::panel2::Panel;
-//use widgets::panel::Stretch;
-
-mod main_window;
-use main_window::make;
-
 use std::sync::Arc;
 
 use druid::lens::{self, LensExt};
@@ -18,40 +9,10 @@ use druid::{
     AppLauncher, Color, Data, Lens, LocalizedString, UnitPoint, Widget,
     WidgetExt, WindowDesc, MenuDesc, MenuItem, Selector,
 };
+use super::AppData;
+use crate::widgets::panel2::Panel;
 
-#[derive(Clone, Data, Lens)]
-struct AppData {
-    channels: Arc<Vec<u32>>,
-    messages: Arc<Vec<u32>>,
-    nicks: Arc<Vec<u32>>,
-    message_text: String,
-}
-
-#[derive(Clone, Data, Lens)]
-struct AppState {
-}
-
-fn main() {
-    // Make the window
-    let main_window = WindowDesc::new(make)
-        .title(LocalizedString::new("roffl").with_placeholder("roffl"))
-        .menu(make_menu());
-
-    // Set our initial data
-    let data = AppData {
-        channels: Arc::new(vec![1, 2, 3, 1,1,2,3,2,1,2,2,3,1,2,2,3,1,2,2,3,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8]),
-        messages: Arc::new(vec![1, 2, 3]),
-        nicks: Arc::new(vec![1, 2]),
-        message_text: String::from(""),
-    };
-
-    AppLauncher::with_window(main_window)
-        .use_simple_logger()
-        .launch(data)
-        .expect("launch failed");
-}
-
-fn make_ui() -> impl Widget<AppData> {
+pub fn make() -> impl Widget<AppData> {
     let mut root = Flex::column();
 
     // Build a button to add children to both lists
@@ -196,65 +157,4 @@ fn make_ui() -> impl Widget<AppData> {
 
     //root.debug_paint_layout()
     root
-}
-
-const MENU_COLOURS_ACTION: Selector = Selector::new("menu-colours-action");
-const MENU_MESSAGING_ACTION: Selector = Selector::new("menu-messaging-action");
-const MENU_CONNECT_ACTION: Selector = Selector::new("menu-server-action");
-const MENU_VIEW_RBAR_ACTION: Selector = Selector::new("menu-right-bar-action");
-const MENU_SEARCH_ACTION: Selector = Selector::new("menu-search-action");
-
-fn make_menu<T: Data>() -> MenuDesc<T> {
-    let edit_menu = MenuDesc::new(LocalizedString::new("common-menu-edit-menu"))
-        .append(druid::platform_menus::common::cut())
-        .append(druid::platform_menus::common::copy())
-        .append(druid::platform_menus::common::paste());
-
-    let settings_menu = MenuDesc::new(LocalizedString::new("Settings"))
-        .append(MenuItem::new(
-            LocalizedString::new("Colours..."),
-            MENU_COLOURS_ACTION
-        ))
-        .append(MenuItem::new(
-            LocalizedString::new("Messaging..."),
-            MENU_MESSAGING_ACTION
-        ))
-        .append(MenuItem::new(
-            LocalizedString::new("About..."),
-            MENU_MESSAGING_ACTION
-        ));
-
-    let view_menu = MenuDesc::new(LocalizedString::new("View"))
-        .append(MenuItem::new(
-            LocalizedString::new("Toggle right panel"),
-            MENU_VIEW_RBAR_ACTION
-        ))
-        .append(MenuItem::new(
-            LocalizedString::new("Toggle left panel"),
-            MENU_VIEW_RBAR_ACTION
-        ));
-
-    let search_menu = MenuDesc::new(LocalizedString::new("Search"))
-        .append(MenuItem::new(
-            LocalizedString::new("Search..."),
-            MENU_SEARCH_ACTION
-        ))
-        .append(MenuItem::new(
-            LocalizedString::new("Build database"),
-            MENU_SEARCH_ACTION
-        ));
-
-    let server_menu = MenuDesc::new(LocalizedString::new("Server"))
-        .append(MenuItem::new(
-            LocalizedString::new("Connect..."),
-            MENU_CONNECT_ACTION
-        ));
-
-    MenuDesc::platform_default()
-        .unwrap_or(MenuDesc::empty())
-        .append(server_menu)
-        .append(edit_menu)
-        .append(view_menu)
-        .append(search_menu)
-        .append(settings_menu)
 }
