0e9ebaaa78b00def07dbbdc044672f338a768543
Ed Cragg <ed.cragg@eipi.xyz>
2020-05-17 22:37:32 +0100

    Make a clean left bar

--- src/gui/src/main_window.rs
+++ src/gui/src/main_window.rs
@@ -3,7 +3,7 @@ use std::sync::Arc;
 use druid::lens::{self, LensExt};
 use druid::widget::{
     Button, CrossAxisAlignment, Flex, Label, List, Scroll,
-    TextBox, Container, Split,
+    TextBox, Container, Split, SizedBox
 };
 use druid::{
     AppLauncher, Color, Data, Lens, LocalizedString, UnitPoint, Widget,
@@ -15,18 +15,8 @@ use crate::widgets::panel2::Panel;
 pub fn make() -> impl Widget<AppData> {
     let mut root = Flex::column();
 
-    // Build a button to add children to both lists
-    root.add_child(
-        Label::new("Channel name")
-            .padding(4.0)
-            .expand_width(),
-    );
-
-    let mut lists = Flex::row().cross_axis_alignment(CrossAxisAlignment::Start);
-
-    // Build the channel list
-    lists.add_flex_child(
-        Scroll::new(List::new(|| {
+    let nick_list = Scroll::new(
+        List::new(|| {
             Label::new(|item: &u32, _env: &_| format!("List item #{}", item))
                 .with_text_size(10.0)
                 .align_vertical(UnitPoint::LEFT)
@@ -36,47 +26,15 @@ pub fn make() -> impl Widget<AppData> {
                 .background(Color::rgb(0.5, 0.5, 0.5))
         }))
         .vertical()
-        .lens(AppData::channels),
-        1.0,
-    );
+        .lens(AppData::nicks);
 
+    let mut right_panel = Flex::column();
 
-    // Build the nick list
-    lists.add_flex_child(
-        Scroll::new(List::new(|| {
-            Label::new(|item: &u32, _env: &_| format!("List item #{}", item))
-                .with_text_size(10.0)
-                .align_vertical(UnitPoint::LEFT)
-                .padding(2.0)
-                .expand()
-                .height(20.0)
-                .background(Color::rgb(0.5, 0.5, 0.5))
-        }))
-        .vertical()
-        .lens(AppData::nicks),
-        1.0,
-    );
+    right_panel.add_flex_child(nick_list, 1.0);
 
-    // Construct the footer
-    let mut footer = Flex::row();
-    footer.add_flex_child(
-        TextBox::new()
-            .padding(2.0)
-            .expand()
-            .align_vertical(UnitPoint::BOTTOM)
-            .lens(AppData::message_text),
-            1.0
-    );
-    footer.add_child(
-        Button::new("Send")
-            .padding(2.0)
-            .align_vertical(UnitPoint::BOTTOM)
-    );
+    let mut left_panel = Flex::column();
 
-    // Add panel layout
-    let mut left_panel_flex = Flex::column();
-
-    let channels_list = Scroll::new(List::new(|| {
+    let channel_list = Scroll::new(List::new(|| {
         Flex::row()
             .with_spacer(10.0)
             .with_flex_child(
@@ -86,34 +44,22 @@ pub fn make() -> impl Widget<AppData> {
                     .padding(2.0)
                     .expand()
                     .height(20.0),
-                    //.background(Color::rgb(0.5, 0.5, 0.5)),
-                    1.0
+                1.0
             )
     }))
     .vertical()
     .lens(AppData::channels);
 
-    /*
-    left_panel_flex.add_flex_child(
-        Label::new("Channels").padding(10.0),
-        0.0
-    );
-    */
-    left_panel_flex.add_flex_child(channels_list, 1.0);
-    left_panel_flex.add_spacer(10.0);
+    left_panel.add_flex_child(channel_list, 1.0);
 
-    let left_panel = Panel::left(left_panel_flex, 150.0)
-        .expand_height();
-
-    let right_panel = Panel::right(lists, 250.0)
-        .expand_height();
-
-    // Build mid section
     let mut midsection = Flex::row();
-    midsection.add_child(left_panel);
 
-    // Add message list
-    // Build the message list with shared data
+    midsection.add_child(
+        SizedBox::new(left_panel)
+            .fix_width(200.0)
+            .background(Color::rgb(0.25, 0.25, 0.25))
+    );
+
     midsection.add_flex_child(
         Scroll::new(List::new(|| {
             Flex::row()
@@ -146,14 +92,31 @@ pub fn make() -> impl Widget<AppData> {
     midsection.add_child(right_panel);
 
     root.add_flex_child(midsection, 1.0);
-    root.add_spacer(2.0);
 
+    let mut footer = Flex::row();
+    footer.add_flex_child(
+        TextBox::new()
+            .padding(2.0)
+            .expand()
+            .align_vertical(UnitPoint::BOTTOM)
+            .lens(AppData::message_text),
+            1.0
+    );
+
+    footer.add_child(
+        Button::new("Send")
+            .padding(2.0)
+            .align_vertical(UnitPoint::BOTTOM)
+    );
+
+    /*
     root.add_child(
         footer
             .fix_height(30.0)
             .padding(2.0)
             .expand_width(),
     );
+*/
 
     //root.debug_paint_layout()
     root
