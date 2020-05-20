8c1daa332c7b57e4d6cfb922727cc83c20fff7ad
Ed Cragg <ed.cragg@eipi.xyz>
2020-05-20 19:44:10 +0100

    Add docopt CLI parsing to make_release utility

--- tools/make_release/src/main.rs
+++ tools/make_release/src/main.rs
@@ -1,4 +1,37 @@
+use docopt::Docopt;
+use serde::Deserialize;
+
+const USAGE: &'static str = "
+rcchat make release utility.
+
+Usage:
+    make_release --all --bump
+    make_release --package FOO --bump
+    make_release --all --bump --major
+    make_release --package FOO --bump --major
+    make_release --help
+    make_release --version
+
+Options:
+    -h --help       Show this screen.
+    --version       Show version.
+    --bump          Bump the package version.
+    --package=FOO   Which package to bump.
+    --major         Make a major release.
+";
+
+#[derive(Debug, Deserialize)]
+struct Args {
+}
+
 fn main() {
-    /// Put git ref in a file
+    // Parse CLI args
+    let args: Args = Docopt::new(USAGE)
+        .and_then(|d| d.version(Some(String::from("0.1.0"))).parse())
+        .and_then(|d| d.deserialize())
+        .unwrap_or_else(|e| e.exit());
+
     println!("Hello, world!");
+
+    /// Put git ref in a file
 }
