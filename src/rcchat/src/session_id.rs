0f4494f8d4bbfcff3e954dbbc72ddfc46967fccc
Ed Cragg <drq.11235@gmail.com>
2020-06-07 01:32:01 +0100

    Add test case and fix bytes/bits

--- src/rcchat/src/session_id.rs
+++ src/rcchat/src/session_id.rs
@@ -2,7 +2,7 @@ use rand_core::{RngCore, OsRng};
 use base64::URL_SAFE;
 
 /// Default number of session ID bytes to generate
-const BYTES: usize = 160;
+const BYTES: usize = 160 / 8;
 
 /// Generate a cryptographically secure session ID with
 /// size of a specified number of bytes, encoded as base64
@@ -16,4 +16,20 @@ pub fn generate_bits(bytes: usize) -> String {
 /// default size, encoded as base64
 pub fn generate() -> String {
     generate_bits(BYTES)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_generate() {
+        let first = generate();
+        let second = generate();
+        assert_ne!(first, second);
+        println!("{}", first);
+        println!("{}", second);
+        assert_eq!(first.len(), 28);
+        assert_eq!(second.len(), 28);
+    }
 }
